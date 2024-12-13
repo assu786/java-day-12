3f09c4b99ed6309e6d3912a86c6ac6fe70082fc1
assu786 <shaikaqsa.78@gmail.com>
2024-12-13 13:59:58 +0530

    Add files via upload

---
+++ hashmap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class A 
+{
+    public static void main(String args[])
+    {
+        HashMap<String,String> a=new HashMap<>();
+        a.put("sno","01");
+        a.put("sname","arjun");
+        System.out.println(a);
+    }
+}
